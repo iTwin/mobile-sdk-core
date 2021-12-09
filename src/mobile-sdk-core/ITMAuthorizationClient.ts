b74e063ef74ecae21b1612f071fc759eee9ec088
Travis.Cobbs <>
2021-12-09 11:00:52 -0800

    Add signOut to ITMAuthorizationClient.

--- src/mobile-sdk-core/ITMAuthorizationClient.ts
+++ src/mobile-sdk-core/ITMAuthorizationClient.ts
@@ -6,7 +6,20 @@ import { AuthorizationClient } from "@itwin/core-common";
 import { Messenger } from "./Messenger";
 
 export class ITMAuthorizationClient implements AuthorizationClient {
+
+  /**
+   * Get the AccessToken of the currently authorized user, or blank string if no token is available.
+   * @returns A promise that resolves to a string representing the access token.
+   */
   public async getAccessToken() {
     return (await Messenger.query("Bentley_ITM_getAccessToken")) as string;
+  }
+
+  /**
+   * Sign the user out.
+   * @returns A promise that resolves to void.
+   */
+  public async signOut() {
+    return (await Messenger.query("Bentley_ITM_signOut")) as void;
   }
 }
