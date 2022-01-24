c019c4c204c34f828c92bf27fe4192810d9dcc2a
Travis.Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2022-01-24 10:18:18 -0800

    Update to use iTwin 3.0.0, and update version to 1.0.0-dev.3.

--- src/mobile-sdk-core/UIError.ts
+++ src/mobile-sdk-core/UIError.ts
@@ -2,7 +2,7 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import { ResponseError } from "@bentley/itwin-client";
+import { ResponseError } from "@itwin/core-frontend/lib/cjs/request/Request";
 import { MessageNotImplementedError } from "./Messenger";
 import { MobileCore } from "./MobileCore";
 
